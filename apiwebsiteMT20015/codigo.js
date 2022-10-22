21a4f7255c31e6eb29d2778f13be34b64787b182
WilfredoMT <91161207+WilfredoMT@users.noreply.github.com>
2022-10-21 19:07:35 -0600

    Add files via upload

--- apiwebsiteMT20015/codigo.js
+++ apiwebsiteMT20015/codigo.js
@@ -1,3 +1,4 @@
+//MT20015 - Wilfredo Jose Mancia Tejada - TPI115 - 61b
 var fila="<tr><td class='id'></td><td class='foto'></td><td class='price'></td><td class='title'></td><td class='description'></td><td class='category'></td></tr>";
 	 var productos=null;
   function codigoCat(catstr) {
